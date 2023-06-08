2f5156f7090eb7ead94aca99fbd7bb2b40ac3ac7
Siddiq <msiddiq1400@gmail.com>
2023-06-09 04:19:37 +0500

    profile added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import MyProfilePic from './components/MyProfilePic'
+import Navbar from './components/Navbar'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="dark:bg-slate-800">
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import MyProfilePic from "./components/MyProfilePic";
+
 export default function Home() {
   return (
-    <main>
-      
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and welcome ✌️ &nbsp;
+        <span className="whitespace-nowrap">
+          I am <span className="font-bold">Siddiq</span>
+        </span>
+      </p>
     </main>
   )
 }
